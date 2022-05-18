9ce9fd1f8c9dfb9c8701459db63d943e3ad5d5fc
Jgardin875 <jess.gardin88@gmail.com>
2022-05-18 14:47:36 -0400

    added tables_ex and updated mysql-intro

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -2,3 +2,10 @@ SELECT * FROM mysql.user;
 SELECT user, host FROM mysql.user;
 SELECT * FROM mysql.help_topic;
 SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
+
+
+--use databse_name;
+--select * from emplyees.employees
+--limit 10;
+--use alboms_db; --move around relative paths
+--select * from albumsdb.albums;  --# aboslute path 
---
+++ tables_exercises.sql
@@ -0,0 +1,17 @@
+use employees;
+show tables;
+describe employees;
+-- #5 int, date, varchar(14), varchar(16), enum('M','F')
+-- #6 Which table(s) do you think contain a numeric type column? dept_emp, dept_manager, employees, salaries, titles
+-- #7 Which table(s) do you think contain a string type column? departments, dept_emp, dept_manager, empployees, titles
+-- #8 Which table(s) do you think contain a date type column? dept_emp, dept_manager, employees, salaries, titles
+-- #9 What is the relationship between the employees and the departments tables? Department number adn emplyee number possibly have some overlap ie one is included in and or used to make the other
+describe employees;
+-- #10
+
+show create table dept_manager;
+
+
+
+
+
