248670afa17c8aede908f0b09529b254fd053b8b
Jgardin875 <jess.gardin88@gmail.com>
2022-05-20 11:30:43 -0400

    added 1 correction from review session

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -16,7 +16,8 @@ select *, datediff(curdate(), hire_date) as tenure from employees
 where birth_date like '%12-25' and hire_date like '199%';
 
 #5 Find the smallest and largest current salary from the salaries table. 
-select min(salary), max(salary) from salaries;
+select min(salary), max(salary) from salaries
+where to_date > now(); #learned line 20 during the review, before review I just had line 19
 
 #For learning purposes:
 
